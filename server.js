350f3b78dbb16be40129c860ad36b904eb5f37d8
qxyzpk <75301132+qxyzpk@users.noreply.github.com>
2025-02-10 23:44:55 +0100

    statystyki

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('users.db')
 
 db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS users (name TEXT, password TEXT)')
+  db.run('CREATE TABLE IF NOT EXISTS users (name TEXT, password TEXT, games INT, wins INT)')
 })
 
 const sessionMiddleware = session({
@@ -35,44 +35,73 @@ app.use(express.static("public"));
 app.use(sessionMiddleware)
 io.engine.use(sessionMiddleware);
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+  if(req.session.log) {
+    req.session.log = false
+    res.render("index", {"guest":false, "text":"Zalogowano pomyślnie"})
+  }
+  else {
+    console.log(sid_to_player.get(req.sessionID))
+    res.render("index", {"guest":sid_to_player.get(req.sessionID)?.guest, "text":""})
+  }
+});
 app.get("/game/:n", (req, res) => {
-  res.render("game")
+  res.render("game", {"guest":sid_to_player.get(req.sessionID)?.guest})
+});
+
+app.get("/profile", (req, res) => {
+  console.log("???", sid_to_player.get(req.sessionID)?.name)
+  db.get('SELECT name, wins, games FROM users WHERE name = ?', sid_to_player.get(req.sessionID)?.name, (err, dbres) =>{
+    if(dbres) {
+      res.render("profile", dbres)
+    }
+    else {
+      res.redirect('/login')
+    }
+  })
 });
 
 app.get("/room/:n", (req, res) => {
-  console.log(roomids, req.params.n)
   if(roomids.has(Number(req.params.n)))
-    res.render("room", {"n":req.params.n})
+    res.render("room", {"n":req.params.n, "guest":sid_to_player.get(req.sessionID)?.guest || false})
   else
     res.send('<img src="https://http.cat/404" />')
 });
 app.get("/login", (req, res) => {
-  res.render("login", ok="")
+  res.render("login", {"ok":"", "guest":false})
 });
 
 app.post("/login", (req, res) => {
   db.get('SELECT name, password FROM users WHERE (name = ? AND password = ?)', req.body.name, req.body.password, (err, dbres) =>{
     if(dbres) {
-      sid_to_player[req.sessionID].name = req.body.name
-      sid_to_player[req.sessionID].guest = false
-      res.render("login", {ok:"ok"})
+      sid_to_player.set(req.sessionID, new Player(req.sessionID, req.body.name, null, false))
+      req.session.log = true
+      res.redirect("/")
     }
-    else
-      res.render("login", {ok:"wrong"})
+    else {
+      res.render("login", {"ok":"Złe dane", "guest":false})
+    }
   })
 });
 
 app.get("/register", (req, res) => {
-  res.render("register")
+  res.render("register", {"ok":"","guest":false})
 });
 
 app.post("/register", (req, res) => {
-  console.log(req.body)
-  const stmt = db.prepare('INSERT INTO users VALUES (?, ?)')
-  stmt.run(req.body.name, req.body.password)
-  stmt.finalize()
-  res.render("register")
+  db.get('SELECT name FROM users WHERE name = ?', req.body.name, (err, dbres) =>{
+    if(dbres) {
+      res.render("register", {"ok":"Istnieje użytkownik o podanej nazwie", "guest":false})
+    }
+    else {
+      req.session.log = true
+      sid_to_player.set(req.sessionID, new Player(req.sessionID, req.body.name, null, false))
+      const stmt = db.prepare('INSERT INTO users VALUES (?, ?, 0, 0)')
+      stmt.run(req.body.name, req.body.password)
+      stmt.finalize()
+      res.redirect("/")
+    }
+  })
 });
 
 class Player {
@@ -109,13 +138,13 @@ function smallest() {
 
 io.on("connect", socket => {
   const sid = socket.request.session.id;
-  if(!sids.has(sid)) {
+  if(!sid_to_player.has(sid)) {
+    console.log("new")
     let name = rand_name()
     names.add(name)
-    sid_to_player[sid] = new Player(sid, name, null, true);
-    sids.add(sid)
+    sid_to_player.set(sid, new Player(sid, name, null, true));
   }
-  let player = sid_to_player[sid]
+  let player = sid_to_player.get(sid)
   let name = player.name
 
   for(const n of roomids) {
@@ -134,20 +163,20 @@ io.on("connect", socket => {
     if(!players[n])
       players[n] = new Set()
     for(const id of players[n]) {
-      socket.emit("newPlayer", sid_to_player[id].name);
+      socket.emit("newPlayer", sid_to_player.get(id).name);
     }
     if(!players[n].has(sid)) {
       players[n].add(sid)
       io.to(`Room${n}`).emit("newPlayer", name);
     }
-    sid_to_player[sid].roomid = n
+    sid_to_player.get(sid).roomid = n
   })
   socket.on("leaveRoom", () => {
-    io.to(`Room${sid_to_player[sid].roomid}`).emit("delPlayer", player.name);
+    io.to(`Room${sid_to_player.get(sid).roomid}`).emit("delPlayer", player.name);
   })
 
   socket.on("start", () => {
-    socket.to(`Room${sid_to_player[sid].roomid}`).emit("start")
+    socket.to(`Room${sid_to_player.get(sid).roomid}`).emit("start")
   })
 });
 
