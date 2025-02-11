827991ba93fdedbd9bf5fce5ca7c780b5675aeec
Sidlee <jakub.szydl@gmail.com>
2025-02-11 05:40:05 +0100

    haha

--- cssfun/proba4/server.js
+++ cssfun/proba4/server.js
@@ -47,6 +47,7 @@ io.on("connection", (socket) => {
     }); 
 
     
+
     
     // let GameStarter = 9; 
     // for(let i = 1; i <= 10; i++){ 
@@ -83,27 +84,58 @@ io.on("connection", (socket) => {
 
     // Wysyłamy aktualny stan planszy nowemu graczowi
     socket.emit("update_board", boardState);
-    // Wysyłamy aktualny stan planszy nowemu klientowi
-    socket.emit("update_board", boardState);
+   
 
     // Obsługa umieszczania karty na planszy
-    socket.on("place_card", ({ fieldId, cardImage, rotation }) => {
-        console.log(`Karta ${cardImage} umieszczona na ${fieldId}, rotacja: ${rotation}`);
-        boardState[fieldId] = { cardImage, rotation };
-        let name = cardImage.slice(-7); 
-
-        let index = userCards[socket.id].indexOf(name); 
-        console.log(name, userCards[socket.id]);
-        if(index !== -1){ 
-            userCards[socket.id].splice(index, 1); 
-        } 
-        console.log(name, userCards[socket.id]); 
+    socket.on("place_card", ({ fieldId, cardImage, rotation }) => { 
+        // fieldId, cardImage, rotation -> 
+        /* 
+        20 + 3 * numer gracza  
+        + 0 kilof 
+        + 1 latarnia 
         
-        io.emit("init_cards", { n: userCards[socket.id].length, cards: userCards[socket.id] });
-        io.emit("update_board", boardState); 
+        + 2 wozek */  
+        
+        const start = 15; // Fixed start index for the card name
+        const end = 18;   // Fixed end index for the card name
+        const name = urlString.slice(start, end);
+        let result = 0;     
+        if(fieldId <= 90){
+            result = game.ruch(fieldId / 11, fieldId %11, name, rotation); 
+        }
+        else 
+        { 
+            result = game.ruch(fieldId - 81, 0, name, rotation);   
+        }
+        for(let i = 1; i <= NumberofPlayers; i++){ 
+            full_layout(result[0], result[1], result[2], result[3], i); 
+            // musisz to komus wysłać 
+        }
+        if(game.check_end()) {
+            game.koniec()
+            game.turn++;
+            if(game.turn == 3)
+                io.emit("end")
+        }
+            
+            
+        // console.log(`Karta ${cardImage} umieszczona na ${fieldId}, rotacja: ${rotation}`);
+        // boardState[fieldId] = { cardImage, rotation };
+        // let name = cardImage.slice(-7); 
+
+        // let index = userCards[socket.id].indexOf(name); 
+        // console.log(name, userCards[socket.id]);
+        // if(index !== -1){ 
+        //     userCards[socket.id].splice(index, 1); 
+        // } 
+        // console.log(name, userCards[socket.id]); 
+        
+        // io.emit("init_cards", { n: userCards[socket.id].length, cards: userCards[socket.id] });
+        // io.emit("update_board", boardState); 
     });
 
-
+// kto -- czyj ruch 
+// k -- czyje karty widac
    function full_layout(kto, plansza, rece, blokady, k){  // k in [0,9]
         let NumberofPlayers = rece.length; 
         for(let i = 1; i <= NumberofPlayers; i++) { 
@@ -112,17 +144,16 @@ io.on("connection", (socket) => {
         } 
         for(let i = 0; i < NumberofPlayers; i++){ 
             let mask_m = 1*blokady[3*i] + 2 * blokady[3*i + 1] + 4*blokady[3*i + 2]; 
-            io.emit("update_blocks", {playerId: i/3 + 1, mask: mask_m}); 
+            io.emit("update_blocks", {playerId: i+  1, mask: mask_m}); 
         }
         socket.emit("update_border", {playerId: kto+1, isGreen: 1}); 
         const updatedCards = rece[k].map(card => card + ".png");
         io.emit("init_cards", {n: updatedCards.length, cards: updatedCards });   
         for(let i = 0; i < 7; i++){ 
             for(let j = 0; j < 11; j++){ 
-                socket.emit("place_card", {
+                socket.emit("set_card", {
                     fieldId: 11*i + j,
                     cardImage: "url(/pictures/" + plansza[i][j] +".png)",
-                    rotation: 0
                 });
             }
         } 
@@ -149,75 +180,77 @@ server.listen(PORT, () => {
 
 
 // testowy call full-layoutu: 
-// full_layout(7, [
-    //     [
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00'
-    //     ],
-    //     [
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'R00', 'F00'
-    //     ],
-    //     [
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00'
-    //     ],
-    //     [
-    //       'F00', 'S04', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'R00', 'F00'
-    //     ],
-    //     [
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00'
-    //     ],
-    //     [
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'R00', 'F00'
-    //     ],
-    //     [
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00', 'F00',
-    //       'F00', 'F00'
-    //     ]
-    //   ], [
-    //     [ 'B02', 'B05', 'E07', 'E02' ],
-    //     [ 'B06', 'E12', 'B01', 'E03' ],
-    //     [ 'E01', 'E07', 'E03', 'E09' ],
-    //     [ 'E15', 'E05', 'E05', 'E15' ],
-    //     [ 'E06', 'E03', 'E01', 'E11' ],
-    //     [ 'E16', 'B07', 'B06', 'E13' ],
-    //     [ 'E07', 'E06', 'B03', 'E05' ],
-    //     [ 'E06', 'E02', 'E04', 'E06' ],
-    //     [ 'B04', 'E05', 'E01', 'E14' ],
-    //     [ 'E01', 'E07', 'E14', 'B05' ]
-    //   ], [
-    //     0, 0, 0, 0, 0, 0, 0, 0, 0,
-    //     0, 0, 0, 0, 0, 0, 0, 0, 0,
-    //     0, 0, 0, 0, 0, 0, 0, 0, 0,
-    //     0, 0, 0
-    //   ], 8); 
-    // console.log("Zrobiona robota!\n"); 
-    // game.init(10);
-    // socket.on("move", (move) => {
-    //     ruch(move)
-    //     render(game)
-    //     if(game.check_end()) {
-    //         game.koniec()
-    //         game.turn++;
-    //         if(game.turn == 3)
-    //             io.emit("end")
-    //     }
-    // });
+/* 
+
+    full_layout(7, [
+        [
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'F00', 'F00'
+        ],
+        [
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'R00', 'F00'
+        ],
+        [
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'F00', 'F00'
+        ],
+        [
+          'F00', 'S04', 'F00',
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'R00', 'F00'
+        ],
+        [
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'F00', 'F00'
+        ],
+        [
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'R00', 'F00'
+        ],
+        [
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'F00', 'F00', 'F00',
+          'F00', 'F00'
+        ]
+      ], [
+        [ 'B02', 'B05', 'E07', 'E02' ],
+        [ 'B06', 'E12', 'B01', 'E03' ],
+        [ 'E01', 'E07', 'E03', 'E09' ],
+        [ 'E15', 'E05', 'E05', 'E15' ],
+        [ 'E06', 'E03', 'E01', 'E11' ],
+        [ 'E16', 'B07', 'B06', 'E13' ],
+        [ 'E07', 'E06', 'B03', 'E05' ],
+        [ 'E06', 'E02', 'E04', 'E06' ],
+        [ 'B04', 'E05', 'E01', 'E14' ],
+        [ 'E01', 'E07', 'E14', 'B05' ]
+      ], [
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0
+      ], 9); 
+    console.log("Zrobiona robota!\n"); 
+    game.init(10);
+    socket.on("move", (move) => {
+        ruch(move)
+        render(game)
+        if(game.check_end()) {
+            game.koniec()
+            game.turn++;
+            if(game.turn == 3)
+                io.emit("end")
+        }
+    }); */ 
